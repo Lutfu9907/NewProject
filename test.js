c588da8250ffcf99ee85333d8a87972c3b44eca6
LUTFU <93095154+Lutfu9907@users.noreply.github.com>
2024-09-19 03:15:10 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,12 @@
+console.log('deneme test.js');
+
+// REPL ortamı
+// read-eval-print-loop
+
+// REPL Komutları
+// help - Tüm komutları listeler.
+// break - Çok satırlı ifadeden çıkar.
+// ctrl + c - Çalışan komutu durdurur.
+// ctrl + d - REPL ortamını sonlandırır.
+// save dosya_adi - REPL ortamındaki kodları dosyaya kaydeder.
+// load dosya_adi - Dosyadaki kodları REPL ortamına alır.
